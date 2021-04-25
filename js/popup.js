e8fc9905bd8bd111f946ee496a12f34df071bf17
史朗出口 <howaito01@gmail.com>
2021-04-25 14:43:36 +0900

    共有用コミット

--- js/popup.js
+++ js/popup.js
@@ -1,29 +1,26 @@
 let btn = document.getElementById("input-memo-js");
 
-console.log(btn);
-
 btn.addEventListener('click', (e) => {
     e.preventDefault();
 
     let element = document.getElementById('memo-text-js');
     let valueCount;
 
-    chrome.storage.local.get (function (value) {
-        valueCount =  Object.keys(value).length + 1;
-        var valueCount =  String(valueCount);
-         console.log("1" + valueCount + JSON.stringify(value));
-    });
+    chrome.storage.local.get(function (value) {
+        valueCount = Object.keys(value).length + 1;
+        valueCount = String(valueCount);
 
-    chrome.storage.local.set({
-        [valueCount]: element.value
-    }, function () {
-         console.log("2" + valueCount);
-    });
+        chrome.storage.local.set({
+            [valueCount]: element.value
+        }, function () {
 
-    chrome.storage.local.get (function (value) {
-         console.log("3" + JSON.stringify(value));
-    });
+            console.log("2" + valueCount);
 
-    chrome.storage.local.clear();
+            chrome.storage.local.get(function (value) {
+                console.log("3" + JSON.stringify(value));
+            });
+        });
+
+    });
 
 }, false);
